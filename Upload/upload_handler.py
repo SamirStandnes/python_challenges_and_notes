cca9c333f82a8fe9af2e6f0505ba32fcc1db27d1
Samir Standnes Abdi <samir@samirs-air.lan>
2019-10-03 22:56:34 +0200

    creat folder mechanisme when direcotry in os path

--- Upload/upload_handler.py
+++ Upload/upload_handler.py
@@ -13,25 +13,37 @@ def upload_handler(gauth, os_folder_path, gdrive_root_folder_id):
 	folder_num = 0
 
 	for dirpath, dirnames, files in os.walk(path):
-		folder_num += 1
+		
 
 
 		print(os.path.isdir(dirpath))
 
+		if (os.path.isdir(dirpath)):
+			print(dirnames)
+			"""
+			folder_meta = {'title': filename, 'parents': [x['id'] for x in [parents[-1]],'mimeType': 'application/vnd.google-apps.folder'}"""
+			#folder_num += 1 
+			#new_folder = drive.CreateFile({'title': dirnames[folder_num], "parents":  [{"id": folder_id}], "mimeType": "application/vnd.google-apps.folder" })
+			#new_folder.Upload()
+			#print(new_folder['id'])
+			#folder_num += 1
 
-#		print('Found directory: {dirpath}, {folder_num}'.format(dirpath=dirpath, folder_num=folder_num))
+
+
+
+
+			#print('Found directory: {dirpath}, {folder_num}'.format(dirpath=dirpath, folder_num=folder_num))
 		for file_name in files:
 
-			print(dirpath, file_name, files)
+			#print(os.path.join(dirpath, file_name))
 
-/Users/samir/Desktop/sync notes/attachments
-/Users/samir/Desktop/sync notes/notes
+			path = os.path.join(dirpath, file_name)
 
 #			print("{file_name} found file in {folder_num}".format(file_name=file_name, folder_num=folder_num)) 
 """
 			#print(dirpath+file_name)
 			new_file = drive.CreateFile({'title': file_name, "parents": [{"kind": "drive#fileLink", "id": folder_id}]})  # Create GoogleDriveFile instance to parentID.
-			new_file.SetContentFile(path+dirnames+file_name) # Set content of the file from given string.
+			new_file.SetContentFile(path) # Set content of the file from given string.
 			print(new_file)
 			new_file.Upload()
 """
